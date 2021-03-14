4859161fc1b2e729dccf7cdaae9be4769f15951d
nolouch <nolouch@gmail.com>
2021-03-14 22:08:16 +0800

    add some scripts
    Signed-off-by: nolouch <nolouch@gmail.com>

---
+++ script/offline_az.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+help() {
+	echo "Usage:"
+	echo "offline_az.sh -p PD_ADDR -k LABEL_KEY -v LABEL_VALUE"
+	echo "Options:"
+	echo "  -p, PD_ADDR is the address of the pd. default: http://127.0.0.1:2379"
+    echo "  -k, LABEL_KEY is the key of the host's label, default: \"\""
+    echo "  -v, LABEL_VALUE is the value of the label, default: \"\""
+	exit -1
+}
+
+PD_ADDR="http://127.0.0.1:2379"
+
+while getopts 'p:k:v:h' OPT; do
+	case $OPT in
+		p) PD_ADDR="$OPTARG";;
+		k) LABEL_KEY="$OPTARG";;
+		v) LABEL_VALUE="$OPTARG";;
+		h) help;;
+		?) help;;
+	esac
+done
+
+
+[ ! -n "$PD_ADDR" ] && help
+[ ! -n "$LABEL_KEY" ] && help
+[ ! -n "$LABEL_VALUE" ] && help
+
+STORE_RAW_DATA=$(tiup ctl pd -u $PD_ADDR store --jq="{stores: [.stores[] | {id: .store.id, address: .store.address, label_key: .store.labels[]?|select(.key == \"$LABEL_KEY\" and .value == \"$LABEL_VALUE\")}]}"|grep -v "Starting")
+STORE_IDS=($(echo $STORE_RAW_DATA| jq ".stores[]|.id"))
+STORE_ADDRS=($(echo $STORE_RAW_DATA| jq ".stores[]|.address"))
+echo "Offline store [${STORE_IDS[*]}]
+address: [${STORE_ADDRS[*]}]
+total ${#STORE_IDS[@]} (y/n)"
+read action
+if [ "$action" != "${action#[Yy]}" ] ;then
+	for i in ${!STORE_IDS[@]}
+	do
+		echo "Offline store ${STORE_IDS[$i]}, address: ${STORE_ADDRS[$i]} "
+		tiup ctl pd -u $PD_ADDR store delete ${STORE_IDS[$i]} >> /tmp/offline_az.log
+	done
+else 
+	echo "Skiped"
+fi
+
+
+echo "Set Replicas number to 3 (y/n)"
+read action
+if [ "$action" != "${action#[Yy]}" ] ;then
+   tiup ctl pd -u $PD_ADDR config set max-replicas 3 >> /tmp/offline_az.log
+   echo "set replicas number to 3"
+   tiup ctl pd -u $PD_ADDR config set replica-schedule-limit 1000  >> /tmp/offline_az.log
+   echo "set replica-schedule-limit to 1000"
+else
+    echo "Skip..."
+fi
+
+echo "Finished, Detail log in: /tmp/offline_az.log"
---
+++ script/up_az.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+help() {
+	echo "Usage:"
+	echo "offline_az.sh -p PD_ADDR -k LABEL_KEY -v LABEL_VALUE"
+	echo "Options:"
+	echo "  -p, PD_ADDR is the address of the pd. default: http://127.0.0.1:2379"
+	echo "  -n, CLUSTER_NAME is the name of the cluster. default: \"\""
+    echo "  -k, LABEL_KEY is the key of the host's label, default: \"\""
+    echo "  -v, LABEL_VALUE is the value of the label, default: \"\""
+	exit -1
+}
+
+PD_ADDR="http://127.0.0.1:2379"
+
+while getopts 'p:n:k:v:h' OPT; do
+	case $OPT in
+		p) PD_ADDR="$OPTARG";;
+		n) CLUSTER_NAME="$OPTARG";;
+		k) LABEL_KEY="$OPTARG";;
+		v) LABEL_VALUE="$OPTARG";;
+		h) help;;
+		?) help;;
+	esac
+done
+
+[ ! -n "$PD_ADDR" ] && help
+[ ! -n "$LABEL_KEY" ] && help
+[ ! -n "$LABEL_VALUE" ] && help
+[ ! -n "$CLUSTER_NAME" ] && help
+
+STORE_RAW_DATA=$( curl "$PD_ADDR/pd/api/v1/stores?state=1&state=0&state=2"  | jq "{stores: [.stores[] | {id: .store.id, address: .store.address,  state_name: .store.state_name,  label_key: .store.labels[]?|select(.key == \"$LABEL_KEY\" and .value == \"$LABEL_VALUE\")}]}")
+STORE_IDS=($(echo $STORE_RAW_DATA| jq ".stores[]|.id"))
+STORE_ADDRS=($(echo $STORE_RAW_DATA| jq ".stores[]|.address"))
+STORE_StateS=($(echo $STORE_RAW_DATA| jq ".stores[]|.state_name"))
+
+for i in ${!STORE_IDS[@]}
+do
+	STORE_ID=${STORE_IDS[$i]}
+	STORE_ADDR=${STORE_ADDRS[$i]}
+    echo "recover the store $STORE_ID, address: $STORE_ADDR (y/n)"
+    read action
+    if [ "$action" != "${action#[Yy]}" ] ;then
+		if  [ ${STORE_StateS[$i]} == "Up" ]; then
+			continue
+		fi
+	    tiup cluster stop $CLUSTER_NAME -N $STORE_ADDR >> /tmp/up_az.log
+		DATA_DIR=$(tiup cluster display csn-test -N ${STORE_ADDRS[$i]}|grep tikv|awk '{print $7}')
+		HOST=$(tiup cluster display csn-test -N ${STORE_ADDRS[$i]}|grep tikv|awk '{print $3}')
+		curl -X POST "$PD_ADDR/pd/api/v1/store/$STORE_ID/state?state=Tombstone" >> /tmp/up_az.log
+		echo "clean the data of the store ${STORE_IDS[$i]}[$HOST:$DATA_DIR]" 
+		tiup cluster exec $CLUSTER_NAME -N $HOST --command "rm -rf $DATA_DIR" >> /tmp/up_az.log
+		tiup cluster start $CLUSTER_NAME -N ${STORE_ADDRS[$i]} &>> /tmp/up_az.log
+    else
+        echo "Skip store ${STORE_IDS[$i]}, address: ${STORE_ADDRS[$i]}..."
+    fi
+done
+
+echo "Set Replicas number to 5 (y/n)"
+read action
+if [ "$action" != "${action#[Yy]}" ] ;then
+   tiup ctl pd -u $PD_ADDR config set max-replicas 5 >> /tmp/up_az.log
+   echo "set replicas number to 5"
+   tiup ctl pd -u $PD_ADDR config set replica-schedule-limit 1000  >> /tmp/up_az.log
+   echo "set replica-schedule-limit to 64"
+else
+    echo "Skip..."
+fi
+echo "Finished, Detail log in: /tmp/up_az.log"
